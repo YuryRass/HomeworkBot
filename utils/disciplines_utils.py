5861535014cd4a1c22d3587d435bfe402baf15f0
YuryRass <superuser@mail.ru>
2023-08-01 13:21:21 +0300

    Add Docstrings

--- utils/disciplines_utils.py
+++ utils/disciplines_utils.py
@@ -1,5 +1,8 @@
+"""
+    Модуль disciplines_utils.py предназначен
+    для работы с учебными дисциплинами
+"""
 import json
-
 from pydantic.json import pydantic_encoder
 
 from model.pydantic.discipline_works import DisciplinesConfig, \
@@ -7,12 +10,24 @@ from model.pydantic.discipline_works import DisciplinesConfig, \
 
 
 def load_disciplines_config(file_path: str) -> DisciplinesConfig:
+    """Конвертирует данные по учебным дисциплинам из JSON файла
+       в pydantic модель и возвращает ее.
+       Параметры:
+       file_path (str): путь до JSON файла.
+    """
     with open(file_path, encoding='utf-8') as json_file:
         data = json.load(json_file)
         return DisciplinesConfig(**data)
 
 
 def disciplines_config_to_json(data: DisciplinesConfig) -> str:
+    """
+        Конвертирует pydantic модель 'Конфигурация дисциплин'
+        в JSON формат в виде строки и возвращает ее.
+        Параметры:
+        data (DisciplinesConfig): конфиг. данные учебн. дисциплин.
+
+    """
     return json.dumps(
         data,
         sort_keys=False,
@@ -24,11 +39,22 @@ def disciplines_config_to_json(data: DisciplinesConfig) -> str:
 
 
 def disciplines_config_from_json(json_data: str) -> DisciplinesConfig:
+    """
+        Возвращает pydantic модель 'Конфигурация дисциплин' из JSON файла.
+        Параметры:
+        json_data (str): конфиг. данные по учебным дисциплинам в JSON формате.
+    """
     data = json.loads(json_data)
     return DisciplinesConfig(**data)
 
 
 def disciplines_works_to_json(data: DisciplineWorksConfig) -> str:
+    """
+        Конвертирование pydantic модели 'Конфигурация работ дисциплины'
+        в JSON формат.
+        Параметры:
+        data (DisciplineWorksConfig): конфиг. данные работ по учебн. дисциплине.
+    """
     return json.dumps(
         data,
         sort_keys=False,
@@ -40,17 +66,34 @@ def disciplines_works_to_json(data: DisciplineWorksConfig) -> str:
 
 
 def load_discipline(downloaded_data: bytes) -> DisciplineWorksConfig:
+    """
+        Загрузка учебной дисциплины и перевод ее данных в pydantic модель
+        Параметры:
+        downloaded_data (bytes): массив данных в байтах по учебной дисциплине.
+    """
     data = json.loads(downloaded_data)
     return DisciplineWorksConfig(**data)
 
 
 def disciplines_works_from_json(json_data: str) -> DisciplineWorksConfig:
+    """
+        Загрузка учебной дисциплины и перевод ее данных в pydantic модель
+        Параметры:
+        json_data (str): массив данных по учебной дисциплине.
+    """
     data = json.loads(json_data)
     return DisciplineWorksConfig(**data)
 
 
 def counting_tasks(discipline: DisciplineWorksConfig) -> int:
+    """
+        Возвращает количество домашних заданий по учебной дисциплине
+        Параметры:
+        discipline (DisciplineWorksConfig): конфиг. данные работ по учебн. дисциплине.
+    """
     result = 0
+    # в рамках некоторой учебной дисциплины пробегаем по всем лаб. работам
+    # и подсчитываем общее количество домашних заданий.
     for it in discipline.works:
         result += it.amount_tasks
     return result
