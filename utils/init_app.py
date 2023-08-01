fc7cd07051854d4394421ebcc727e57e85d831a6
YuryRass <superuser@mail.ru>
2023-08-01 22:38:28 +0300

    Add docstrings

--- utils/init_app.py
+++ utils/init_app.py
@@ -1,4 +1,8 @@
 import os
+"""
+    Модуль init_app.py создает и инициализирует
+    основную и промежуточную базы данных
+"""
 from distutils.util import strtobool
 from pathlib import Path
 
@@ -10,7 +14,12 @@ from model.pydantic.db_creator_settings import DbCreatorSettings
 
 
 def init_app() -> None:
+    """Создание и инициализация баз данных"""
+
+    # загрузка файла .env
     load_dotenv()
+
+    # создание и инициализация основной БД
     db_is_created = os.path.exists(os.getenv('DATABASE_NAME')+'.sqlite')
     if not db_is_created:
         settings = DbCreatorSettings(
@@ -21,9 +30,11 @@ def init_app() -> None:
         )
         create_main_db(settings)
 
+    # создание и инициализация промежуточной БД
     db_is_created = os.path.exists(os.getenv('QUEUE_DB_NAME') + '.sqlite')
     if not db_is_created:
         create_queue_db()
 
+    # создание директории для отчетов о студентах
     path = Path.cwd()
     Path(path.joinpath(os.getenv('TEMP_REPORT_DIR'))).mkdir(parents=True, exist_ok=True)
