0495ec34bcc1aa0008618a8c5148d2a3db7bffdd
YuryRass <superuser@mail.ru>
2023-08-23 22:13:10 +0300

    Create tests

--- tests/test_utils/conftest.py
+++ tests/test_utils/conftest.py
@@ -5,43 +5,49 @@ import pytest
 from pathlib import Path
 
 
-@pytest.fixture
-def out_dir():
-    # директория, куда будут помещаться распакованные файлы
-    out_dir = 'out'
-    out_dir = Path.cwd().joinpath('tests').joinpath(out_dir)
-    Path.mkdir(out_dir, parents=True, exist_ok=True)
-    return out_dir
+class CheckExistTestFolder:
+    @pytest.fixture
+    def out_dir(self):
+        # директория, куда будут помещаться распакованные файлы
+        out_dir = 'out'
+        out_dir = Path.cwd().joinpath('tests').joinpath(out_dir)
+        Path.mkdir(out_dir, parents=True, exist_ok=True)
+        return out_dir
 
+    @pytest.fixture
+    def zip_file(self):
+        # создаем архив in.zip с пятью пустыми файлами
+        zip_file = 'in.zip'
+        zip_file = Path.cwd().joinpath('tests').joinpath(zip_file)
 
-@pytest.fixture
-def zip_file():
-    # создаем архив in.zip с пятью пустыми файлами
-    zip_file = 'in.zip'
-    zip_file = Path.cwd().joinpath('tests').joinpath(zip_file)
+        with ZipFile(zip_file, 'w') as my_zip:
+            for i in range(5):
+                fname = Path.cwd().joinpath('tests').joinpath(f'file_{i}.py')
+                open(fname, 'w')
+                my_zip.write(
+                    fname, os.path.relpath(
+                        fname, Path.cwd().joinpath('tests')
+                    )
+                )
 
-    with ZipFile(zip_file, 'w') as my_zip:
+        return zip_file
+
+    @pytest.fixture
+    def downloaded_file(self, zip_file):
+        # сырое представление архива in.zip
+        with open(zip_file, 'rb') as myzip:
+            downloaded_file = myzip.read()
+        return downloaded_file
+
+    @pytest.fixture
+    def delete_all_files(self, out_dir, zip_file):
+        yield
+        # удаляем все созданные раннее файлы
         for i in range(5):
             fname = Path.cwd().joinpath('tests').joinpath(f'file_{i}.py')
-            open(fname, 'w')
-            my_zip.write(
-                fname, os.path.relpath(
-                    fname, Path.cwd().joinpath('tests')
-                )
-            )
-
-    return zip_file
-
-
-@pytest.fixture
-def delete_all_files(out_dir, zip_file):
-    yield
-    # удаляем все созданные раннее файлы
-    for i in range(5):
-        fname = Path.cwd().joinpath('tests').joinpath(f'file_{i}.py')
-        if os.path.isfile(fname):
-            os.remove(fname)
-    if os.path.isfile(zip_file):
-        os.remove(zip_file)
-    if os.path.isdir(out_dir):
-        rmtree(out_dir)
+            if os.path.isfile(fname):
+                os.remove(fname)
+        if os.path.isfile(zip_file):
+            os.remove(zip_file)
+        if os.path.isdir(out_dir):
+            rmtree(out_dir)
--- tests/test_utils/test_unzip_homework_files.py
+++ tests/test_utils/test_unzip_homework_files.py
@@ -2,18 +2,16 @@ import os
 import pytest
 
 from utils.unzip_test_files import save_test_files
+from conftest import CheckExistTestFolder
 
-pytestmark = pytest.mark.usefixtures("delete_all_files")
 
+class TestCheckExistTestFolder(CheckExistTestFolder):
+    pytestmark = pytest.mark.usefixtures("delete_all_files")
 
-@pytest.mark.asyncio
-async def test_save_test_files(out_dir, zip_file):
-    # сырое представление архива in.zip
-    with open(zip_file, 'rb') as myzip:
-        downloaded_file = myzip.read()
+    @pytest.mark.asyncio
+    async def test_save_test_files(self, out_dir, downloaded_file):
+        # помещаем в папку out распакованный архив
+        await save_test_files(out_dir, downloaded_file)
 
-    # помещаем в папку out распакованный архив
-    await save_test_files(out_dir, downloaded_file)
-
-    assert os.listdir(out_dir) == \
-        ['archive.zip'] + [f'file_{i}.py' for i in range(5)]
+        assert os.listdir(out_dir) == \
+            ['archive.zip'] + [f'file_{i}.py' for i in range(5)]
