f54460a754f908990c52798e8b42a89645830327
YuryRass <72935703+YuryRass@users.noreply.github.com>
2023-09-28 19:28:24 +0300

    Delete _disciplines\tmp\answer/Z4831/7/Рассказова Вера Викторовна-1771906022 directory

--- _disciplines\tmp\answer/Z4831/7/Рассказова Вера Викторовна-1771906022/lab7_7.py
+++
@@ -1,9 +0,0 @@
-"""Вычислите выражение (val1 + val2) / (val2 - val3)"""
-
-
-def app():
-    val1 = int(input())
-    val2 = int(input())
-    val3 = int(input())
-
-    print((val1 + val2) / (val2 - val3))
